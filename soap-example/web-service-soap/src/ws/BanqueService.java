7dd70ed028b68b164dd2d102407d637f40326ba6
omaarouf <Ouassime.MAAROUF@um6p.ma>
2022-12-11 14:19:50 +0100

    add rapport

--- soap-example/web-service-soap/src/ws/BanqueService.java
+++ soap-example/web-service-soap/src/ws/BanqueService.java
@@ -1,27 +1,21 @@
 package ws;
-
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-
 // POJO - Plain Old Java Object
 @WebService(serviceName = "BanqueWS")
 public class BanqueService {
-
     @WebMethod(operationName = "Convert")
     public double conversion(@WebParam(name = "montant") double montant) {
         return montant * 10.5;
     }
-
     @WebMethod
     public Compte getCompte(@WebParam(name = "code") int code) {
         return new Compte(code, Math.random() * 9000 + 1000, new Date());
     }
-
     @WebMethod
     public List<Compte> listComptes() {
        List<Compte> comptes = new ArrayList<Compte>();
--- soap-example/web-service-soap/src/ws/Compte.java
+++ soap-example/web-service-soap/src/ws/Compte.java
@@ -1,26 +1,21 @@
 package ws;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import java.util.Date;
-
 @XmlRootElement(name = "compte")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Compte {
     private int code;
     private double solde;
-
     @XmlTransient
     private Date dateCreation;
-
     public Compte(int code, double solde, Date dateCreation) {
         this.code = code;
         this.solde = solde;
         this.dateCreation = dateCreation;
     }
-
     public Compte(){}
 
     public int getCode() {
